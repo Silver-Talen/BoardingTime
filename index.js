6461593811110c4165b1ea83331d76fc32429972
Ace <1kingdom4all@gmail.com>
2019-03-01 11:04:43 -0700

    Users

--- index.js
+++ index.js
@@ -16,4 +16,14 @@ var urlencodedParser = bodyParser.urlencoded({
 
 app.get('/', routes.index);
 
+app.get('/create', route.create);
+app.post('/create', urlencodedParser, route.createPerson);
+
+app.get('/edit/:id', route.edit);
+app.post('/edit/:id', urlencodedParser, route.editPerson)
+
+app.get('/delete/:id', route.delete);
+
+app.get('/details/:id', route.details);
+
 app.listen(3000);
