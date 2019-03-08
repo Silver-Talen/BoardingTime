d8c78adca6f4ffad321765044924b00b47f0e213
Ace <1kingdom4all@gmail.com>
2019-03-08 12:05:39 -0700

    Update routes

--- index.js
+++ index.js
@@ -6,17 +6,15 @@ bodyParser = require('body-parser');
 
 var app = express();
 
-const checkAuth = (req, res, next) => {
-    if(req.session.user && req.session.user.isAuthenticated){
-      next();
-    }else{
-      res.redirect('/');
-    }
-  }
-
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
+
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(expressSession({
+  secret: 'Whatever54321',
+  saveUninitialized: true,
+  resave: true
+}));
 
 var urlencodedParser = bodyParser.urlencoded({
     extended: true
@@ -37,6 +35,6 @@ app.get('/details/:id', routes.details);
 app.get('/admin', routes.admin);
 
 app.get('/logout', routes.logout);
-app.post('/login',urlencodedParser, routes.login);
+app.post('/login', urlencodedParser, routes.login);
 
 app.listen(3000);
