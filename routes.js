dc5f43747edd7fa8b92701926f0336302a543902
Eric <supersneakyfox@gmail.com>
2019-03-11 14:33:42 -0600

    errors :D
    again

--- routes.js
+++ routes.js
@@ -86,36 +86,38 @@ exports.createMessage = (req, res) => {
 exports.edit = (req, res) => {
     var query = Account.findOne({username: username}, (err, user) => {
         if (err) return handleError(err);
+        console.log(user.avatar_eyes);
         res.render('edit', {
             title: 'Edit',
             "session": activeSession,
-            user_id: user._id
+            user_id: user._id,
+            avatar_eyes: user.avatar_eyes,
+            avatar_mouth: user.avatar_eyes,
+            avatar_nose: user.avatar_eyes,
+            avatar_color: user.color,
+            username: user.username,
+            age: user.age,
+            email: user.email
         });
     });
 }
 
 exports.editPerson = (req, res) => {
-    var hash = bcrypt.hashSync(req.body.password);
-    var regexColor = req.body.color;
-    regexColor = regexColor.replace(/[#]/g, '');
-    var account = new Account({
-        username: req.body.username,
-        age: req.body.age,
-        password: hash,
-        userLevel: req.body.userLevel,
-        email: req.body.email,
-        avatar_eyes: req.body.avatar_eyes,
-        avatar_nose: req.body.avatar_nose,
-        avatar_mouth: req.body.avatar_mouth,
-        color: regexColor
-    });
-    account.save((err, account) => {
-        if(err) return console.error(err);
-        console.log(account.username + ' added')
-    });
-    Account.findByIdAndRemove(req.params.id, (err, account) => {
-        if(err) return console.error(err);
-        res.redirect('/');
+    var query = Account.findOne({username: username}, (err, user) => {
+        if (err) return handleError(err);
+        var hash = bcrypt.hashSync(req.body.password);
+        var regexColor = req.body.color;
+        regexColor = regexColor.replace(/[#]/g, '');
+        user.username = req.body.username;
+        user.age = req.body.age;
+        user.password = hash;
+        user.userLevel = req.body.userLevel;
+        user.email = req.body.email;
+        user.avatar_eyes = req.body.avatar_eyes;
+        user.avatar_nose = req.body.avatar_nose;
+        user.avatar_mouth = req.body.avatar_mouth;
+        user.color = regexColor;
+        res.redirect('/account');
     });
 }
 
@@ -146,30 +148,35 @@ exports.login = (req, res) => {
 }
 
 exports.authenticateUser = (req, res) => {
-    Account.find({username: req.body.username}, (err, account) => {
-        if(err) console.error(err);
-        if(req.body.username==account[0].username && bcrypt.compareSync(req.body.password, account[0].password)){
-            activeSession = true;
-            req.session.user={
-              isAuthenticated: true,
-              username: req.body.username,
-            };
-            username = req.session.user.username;
-            userLevel = account[0].userLevel;
-        }
-        else{
-            console.log("Not authenticated");
-        }
-    });
-    res.redirect('/');
+    try {
+        Account.find({username: req.body.username}, (err, account) => {
+            if(err) console.error(err);
+            if(req.body.username==account[0].username && bcrypt.compareSync(req.body.password, account[0].password)){
+                activeSession = true;
+                req.session.user={
+                  isAuthenticated: true,
+                  username: req.body.username,
+                };
+                username = req.session.user.username;
+                userLevel = account[0].userLevel;
+            }
+            else{
+                console.log("Not authenticated");
+            }
+        });
+        res.redirect('/');
+    } catch (error) {
+        res.redirect('/');
+        alert("Gage EEEEE Boy couldn't find your username or password.");
+    }
 }
 
 exports.account = (req, res) => {
     var query = Account.findOne({username: username}, (err, user) => {
         if (err) return handleError(err);
-        console.log("-------------------");
+        console.log("Acount-------------");
         console.log(user);
-        console.log(user.username);
+        console.log("-------------------");
         res.render('account', {
             title: 'Account',        
             avatar_eyes: user.avatar_eyes,
